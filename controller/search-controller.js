8ffe99aef249517b2133199124c10b7f5f05c55a
Arvind Pandey <arvind3157@gmail.com>
2020-03-25 18:40:58 +0530

    [add] post search api

--- controller/search-controller.js
+++ controller/search-controller.js
@@ -1,5 +1,11 @@
+const search_service = require('../services/search');
+
 const search = (req, res) => {
-    console.log('hello');
+    if(req.body === undefined || req.body.key === undefined) {
+        res.json({'Error': 'Invalid request format'})
+    }
+    let result = search_service.search(req.body.key,req.body.k||5);
+    res.json({ result });
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const router = require('./routes');
 
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+
 //  Connect all our routes to our application
 app.use('/', router);
-
 app.listen(3001, function() {
-  console.log('Serving on port 3001');
+  console.log('Server started successfully. Listening on port 3001');
 });
