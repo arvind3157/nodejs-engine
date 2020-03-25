63b096809e065b5df291d9022497e85460147ace
Arvind Pandey <arvind3157@gmail.com>
2020-03-25 17:40:11 +0530

    [fix] to index the title as well

--- services/search.js
+++ services/search.js
@@ -12,7 +12,7 @@ let exclude_words =
         'is','i','you','us','about', 'on', 'in',
         'upon', 'for', 'to','me', 'we','my','he','she',
         'should','than','that','yet','as','no','at','be','too',
-        'more','was', 'also', 'it','by','which','the'
+        'more','was', 'also', 'it','by','which'
     ];
 
 /**
@@ -21,50 +21,25 @@ let exclude_words =
     'token':[[record_id1,record_id2],[frequency,frequency]]
  }
  */
-let map = new Map();
 
-// Attach score to each result based on certain criteria
-// number of token match has highest weightage (n*0.05)
-// number of times token appeared has 2nd highest weightage (m*0.02)
-// final_score = base_score+n*0.05+m*0.02
-function applyScore(result, str) {
-    let tokens = str.split(' ');
-    for(let i=0;i<result.length;i++) {
-        result[i].score = 0;
-        for(let j=0;j<tokens.length;j++) { // Calculates highest number of token match
-            if(map.has(tokens[j])) {
-                let m = map.get(tokens[j]);
-                if(m[0].indexOf(result[i].id)>0) {
-                    result[i].score = result[i].score+0.05; // Keep adding score of 0.05 for every match
-                    let index = m[0].indexOf(result[i].id);
-                    result[i].score = result[i].score+m[1][index]*0.02; // Multiply with frequency of the token
-                }
-            }
-        }
-    }
-    return result;
-}
+let map = new Map(); // global map variable to store inverted index
 
-function sortByScore(result) {
-    return result.sort((a,b)=> a.score < b.score ? 1 : -1)
-}
-
-function preProcess(summaries) {
-    // deep cloning to avoid modification of the original array
-    let s = JSON.parse(JSON.stringify(summaries));
-    for(let i =0;i<s.length;i++) {
-        let arr = s[i].summary.toLowerCase().split(' ');
+function preProcess(summaries, titles) {
+    let s = JSON.parse(JSON.stringify(summaries)); // deep cloning to avoid modification of the original array
+    let t = JSON.parse(JSON.stringify(titles));
+    for(let i =0;i<s.length;i++) { // indexing summary tokens
+        let arr = s[i].summary.toLowerCase().split(' '); // converting to all lowercase
         for(let j=0;j<arr.length;j++) {
-            if(exclude_words.indexOf(arr[j]) === -1) {
-                if(map.has(arr[j])) {
-                    let aa = map.get(arr[j]);
-                    if(aa[0].indexOf(s[i].id)<0) {
-                        aa[0].push(s[i].id);
-                        aa[1].push(1);
+            if(exclude_words.indexOf(arr[j]) === -1) { // excluding index words
+                if(map.has(arr[j])) { // if map already has the tokens
+                    let aa = map.get(arr[j]); // get the token array
+                    if(aa[0].indexOf(s[i].id)<0) { // The given document id is listed or not
+                        aa[0].push(s[i].id); // if not, create entry
+                        aa[1].push(1); // mark the count as 1
                     } else {
-                        let index = aa[0].indexOf(s[i].id);
-                        let count = aa[1][index];
-                        count = count+1;
+                        let index = aa[0].indexOf(s[i].id); // if document id already found
+                        let count = aa[1][index]; // get the count
+                        count = count+1; // increament the count
                         aa[1][index] = count;
                     }
                 } else {
@@ -73,11 +48,28 @@ function preProcess(summaries) {
             }
         }
     }
+    for(let i=0;i<t.length;i++) { // indexing title tokens, follows the similar logic to index title as well
+        let arr = t[i].toLowerCase().split(' ');
+        for(let k=0;k<arr.length;k++) {
+            if(exclude_words.indexOf(arr[k]) === -1) {
+                if(map.has(arr[k])) {
+                    let aa = map.get(arr[k]);
+                    if(aa[0].indexOf(i)<0) {
+                        aa[0].push(i);
+                        aa[1].push(1);
+                    } else {
+                        let index = aa[0].indexOf(i);
+                        let count = aa[1][index];
+                        count = count+1;
+                        aa[1][index] = count;
+                    }
+                } else {
+                    map.set(arr[k],[[i],[1]]);
+                }
+            }
+        }
+    }
     return map;
-}
-
-function unique(value, index, self) {
-    return self.indexOf(value) === index;
 }
 
 const search = (query, k=5) => {
@@ -102,8 +94,40 @@ const search = (query, k=5) => {
     return result.slice(0, k); // Pick top k relevant result
 }
 
-preProcess(summaries);
+// Attach score to each result based on certain criteria
+// number of token match has highest weightage (n*0.05)
+// number of times token appeared has 2nd highest weightage (m*0.02)
+// final_score = base_score+n*0.05+m*0.02
+function applyScore(result, str) {
+    let tokens = str.split(' ');
+    for(let i=0;i<result.length;i++) {
+        result[i].score = 0;
+        for(let j=0;j<tokens.length;j++) { // Calculates highest number of token match
+            if(map.has(tokens[j])) {
+                let m = map.get(tokens[j]);
+                if(m[0].indexOf(result[i].id)>0) {
+                    result[i].score = result[i].score+0.05; // Keep adding score of 0.05 for every match
+                    let index = m[0].indexOf(result[i].id);
+                    result[i].score = result[i].score+m[1][index]*0.02; // Multiply with frequency of the token
+                }
+            }
+        }
+    }
+    return result;
+}
+
+// Sort result by score
+function sortByScore(result) {
+    return result.sort((a,b)=> a.score < b.score ? 1 : -1)
+}
+
+function unique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+preProcess(summaries, titles); // Preprocess the data when module loads
 
 module.exports = {
-    search
+    search,
+    preProcess
 }
--- test/search.test.js
+++ test/search.test.js
@@ -3,3 +3,11 @@ const search = require('../services/search');
 test('search correctly - empty result for empty string', () => {
     expect(search.search('')).toMatchObject([]);
 })
+
+test('search correctly - non-empty result for non-empty string', () => {
+    expect(search.search('the problem is')[0].id).toBe(18);
+})
+
+test('search correctly - get top k results only', () => {
+    expect(search.search('the problem is',5).length).toBe(5);
+})
